bd470aa7a4a66ab04783a19ccc3cdd7f8b4f5455
andrewhamili <rockr1521@gmail.com>
2016-12-13 21:30:08 +0800

    Username and Password textboxes with codes for effect.

--- FBChatOfflineArchive/Form1.Designer.vb
+++ FBChatOfflineArchive/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class LoginForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,11 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LoginForm))
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -39,31 +41,54 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.FBChatOfflineArchive.My.Resources.Resources.fb2016
-        Me.PictureBox1.Location = New System.Drawing.Point(128, 60)
+        Me.PictureBox1.Location = New System.Drawing.Point(129, 60)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(99, 99)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
-        'Form1
+        'txtUsername
+        '
+        Me.txtUsername.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.txtUsername.Location = New System.Drawing.Point(93, 191)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(168, 27)
+        Me.txtUsername.TabIndex = 2
+        Me.txtUsername.Text = "Email or Phone"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.txtPassword.Location = New System.Drawing.Point(93, 235)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(168, 27)
+        Me.txtPassword.TabIndex = 3
+        Me.txtPassword.Text = "Password"
+        '
+        'LoginForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(90, Byte), Integer), CType(CType(152, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(366, 347)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Panel1)
         Me.Font = New System.Drawing.Font("Tahoma", 12.0!)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-        Me.Name = "Form1"
+        Me.Name = "LoginForm"
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Panel1 As System.Windows.Forms.Panel
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
 
 End Class
--- FBChatOfflineArchive/Form1.vb
+++ FBChatOfflineArchive/Form1.vb
@@ -1,3 +1,36 @@
-﻿Public Class Form1
+﻿Public Class LoginForm
 
+    Private Sub LoginForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+
+    End Sub
+
+    Private Sub txtUsername_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtUsername.GotFocus
+        txtUsername.ForeColor = System.Drawing.SystemColors.WindowText
+        If txtUsername.Text = "Email or Phone" Then
+            txtUsername.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub txtUsername_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtUsername.LostFocus
+        txtUsername.ForeColor = System.Drawing.SystemColors.ControlDark
+        If txtUsername.Text = "" Then
+            txtUsername.Text = "Email or Phone"
+        End If
+    End Sub
+
+    Private Sub txtPassword_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtPassword.GotFocus
+        txtPassword.ForeColor = System.Drawing.SystemColors.WindowText
+        If txtPassword.Text = "Password" Then
+            txtPassword.Text = ""
+        End If
+    End Sub
+
+    Private Sub txtPassword_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtPassword.LostFocus
+        txtPassword.ForeColor = System.Drawing.SystemColors.ControlDark
+        If txtPassword.Text = "" Then
+            txtPassword.Text = "Password"
+        End If
+    End Sub
 End Class
